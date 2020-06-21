ecd9345fe711ab46a0cef58bf122b3ec98af10ae
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-21 14:56:41 -0300

    Implementando index de cursoController

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Curso;
 
 class CursoController extends Controller
 {
-    //
+    public function index()
+    {
+        $registros = Curso::all();
+        return view('admin.cursos.index', compact('registros'));
+    }
 }
