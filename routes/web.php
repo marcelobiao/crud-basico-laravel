47778993ad25c2cc5d2c4af66b47c5fd2bc4a3eb
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-21 11:56:44 -0300

    Adicionando rotas de cursos

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contrato/{id}', function ($id) {
+    return "Contrato = $id";
+});
+Route::post('/contrato', function () {
+    var_dump($_POST);
+    return "Contrato: POST";
+});
+Route::put('/contrato', function () {
+    return "Contrato: Put";
+});
+
+Route::get('/admin/cursos',
+    ['as' => 'admin.cursos', 'uses' => 'Admin/CursosController@index']);
+
+Route::get('/admin/cursos/adicionar',[
+    'as' => 'admin.cursos.adicionar', 'uses' => 'Admin/CursosController@adicionar']);
+
+Route::post('/admin/cursos/salvar',
+    ['as' => 'admin.cursos.salvar', 'uses' => 'Admin/CursosController@salvar']);
+
+Route::get('/admin/cursos/editar/{id}',
+    ['as' => 'admin.cursos.editar', 'uses' => 'Admin/CursosController@editar']);
+
+Route::put('/admin/cursos/atualizar/{id}',
+    ['as' => 'admin.cursos.atualizar', 'uses' => 'Admin/CursosController@atualizar']);
+
+Route::get('/admin/cursos/deletar/{id}',
+    ['as' => 'admin.cursos.deletar', 'uses' => 'Admin/CursosController@deletar']);
