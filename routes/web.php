af71a4aeac36f570d2f1c72d0eec7c85fcf1d00a
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-21 14:55:26 -0300

    Corrigindo rotas de curso

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,34 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/contrato/{id}', function ($id) {
     return "Contrato = $id";
 });
+
 Route::post('/contrato', function () {
     var_dump($_POST);
     return "Contrato: POST";
 });
+
 Route::put('/contrato', function () {
     return "Contrato: Put";
 });
 
 Route::get('/admin/cursos',
-    ['as' => 'admin.cursos', 'uses' => 'Admin/CursosController@index']);
+    ['as' => 'admin.cursos', 'uses' => 'Admin\CursoController@index']);
 
 Route::get('/admin/cursos/adicionar',[
-    'as' => 'admin.cursos.adicionar', 'uses' => 'Admin/CursosController@adicionar']);
+    'as' => 'admin.cursos.adicionar', 'uses' => 'Admin\CursoController@adicionar']);
 
 Route::post('/admin/cursos/salvar',
-    ['as' => 'admin.cursos.salvar', 'uses' => 'Admin/CursosController@salvar']);
+    ['as' => 'admin.cursos.salvar', 'uses' => 'Admin\CursoController@salvar']);
 
 Route::get('/admin/cursos/editar/{id}',
-    ['as' => 'admin.cursos.editar', 'uses' => 'Admin/CursosController@editar']);
+    ['as' => 'admin.cursos.editar', 'uses' => 'Admin\CursoController@editar']);
 
 Route::put('/admin/cursos/atualizar/{id}',
-    ['as' => 'admin.cursos.atualizar', 'uses' => 'Admin/CursosController@atualizar']);
+    ['as' => 'admin.cursos.atualizar', 'uses' => 'Admin\CursoController@atualizar']);
 
 Route::get('/admin/cursos/deletar/{id}',
-    ['as' => 'admin.cursos.deletar', 'uses' => 'Admin/CursosController@deletar']);
+    ['as' => 'admin.cursos.deletar', 'uses' => 'Admin\CursoController@deletar']);
