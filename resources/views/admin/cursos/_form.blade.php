327c1b090930728f68111a49faae734f7ad3bfc0
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-22 15:03:29 -0300

    Corrigindo bugs nas view de cursos

--- resources/views/admin/cursos/_form.blade.php
+++ resources/views/admin/cursos/_form.blade.php
@@ -31,7 +31,7 @@
 
 <div class="input-field">
     <p>
-        <input type="checkbox" id="test5" {{isset($registro->publicado) && $registro->publicado == 'sim' ? 'checked' : ''}} value="true" />
+        <input type="checkbox" id="test5" name="publicado" {{isset($registro->publicado) && $registro->publicado == 'sim' ? 'checked' : ''}} value="true" />
         <label for="test5">Publicar?</label>
     </p>
     <br><br>
