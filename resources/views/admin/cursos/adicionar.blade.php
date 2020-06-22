8beb3fb8b8b9e90a44604bce5895f02c64d4113b
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-22 14:44:47 -0300

    Criando view para adição de curso

---
+++ resources/views/admin/cursos/adicionar.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.site')
+
+@section('titulo','Cursos')
+
+@section('conteudo')
+    <div class="container">
+        <h3 class="center">Adicionar cursos</h3>
+        <div class="row">
+            <form class="" action="{{route('admin.cursos.salvar')}}" class="post" enctype="multipart/form-data">
+                {{csrf_field()}}
+                @include('admin.cursos._form')
+
+                <button class="btn deep-orange">Salvar</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
