5a44530122a357250bb300051e7345aa8a47f8df
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-22 15:03:29 -0300

    Corrigindo bugs nas view de cursos

--- resources/views/admin/cursos/adicionar.blade.php
+++ resources/views/admin/cursos/adicionar.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <h3 class="center">Adicionar cursos</h3>
         <div class="row">
-            <form class="" action="{{route('admin.cursos.salvar')}}" class="post" enctype="multipart/form-data">
+            <form class="" action="{{route('admin.cursos.salvar')}}" method="post" enctype="multipart/form-data">
                 {{csrf_field()}}
                 @include('admin.cursos._form')
 
