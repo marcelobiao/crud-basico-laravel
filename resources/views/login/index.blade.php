614fbdfbae5aa7d3aee6822f15139a7832ea511a
Marcelo Biao <marcelobiao2@gmail.com>
2020-06-23 17:18:09 -0300

    Criando index de login

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.site')
+
+@section('titulo','Cursos')
+
+@section('conteudo')
+    <div class="container">
+        <h3 class="center">Entrar</h3>
+        <div class="row">
+            <form class="" action="{{route('site.login.entrar')}}" method="post">
+                {{csrf_field()}}
+
+                <div class="input-field">
+                    <label>Email</label>
+                    <input type="text" name="email">
+                </div>
+                <div class="input-field">
+                    <label>Senha</label>
+                    <input type="text" name="senha">
+                </div>
+
+                <button class="btn deep-orange">Entrar</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
